7179111331c72bb059206531d7ccead9d46e44ce
danielgural <28dang28@gmail.com>
2023-10-31 20:12:57 -0400

    Added Video semantic search

--- README.md
+++ README.md
@@ -16,9 +16,39 @@ fiftyone plugins download https://github.com/danielgural/semantic_video_search
 
 ### `semantic_video_search`
 
-Sorts based on a prompt through your video dataset for the most similar frames or videos by using a similiarty index of your choice. You can even use Vector DB backends such as Qdrant, Pinecone, Milvus, or LanceDB. Sort your frames as seen below, or by full video!
+Sorts based on a prompt through your video dataset for the most similar videos by using a similiarty index of your choice. Use [Twelve Labs](https://twelvelabs.io/) to create an index of your videos to do video semantic search. You can generate the index with the `create semantic video index` operator. Search on four different embeddings: visual, text in video, conversations, and logos.
+
+For more info on Twelve Labs Search, look [here](https://docs.twelvelabs.io/docs/search-single-queries)! Twelve Labs features a free tier so its easy to get started right away!
+
+### `semantic_frames_search` 
+
+Sorts based on a prompt through your video dataset using single frame embeddings. You will need to generate a similarity index with FiftyOne Brain with textual embeddings before hand. You can even use Vector DB backends such as Qdrant, Pinecone, Milvus, or LanceDB for frame based search. Sort your frames as seen below, or by full video!
 
 ![frames](https://github.com/danielgural/semantic_video_search/blob/main/assets/sort_by_frames.gif)
+
+### `create_semantic_video_index`
+
+Generates a [Twelve Labs](https://twelvelabs.io/) similarity index for you video dataset. You can change multiple parameters such as the types of embeddings to search on. The index must contain the embedding type in order to search on. You can also decide to upload your entire dataset, selected samples, or the current view to the database. It is highly recommended you [delegate](https://docs.voxel51.com/plugins/using_plugins.html#setting-up-an-orchestrator) this operator due to its long runtime. Also note that videos must be at least four seconds long!
+
+## Video Semantic Search
+
+To start using the Video Semantic Search, first generate a [Twelve Labs API Token](https://dashboard.twelvelabs.io/home). The token will be used by the plugin to access the Twelve Labs API. To set these variables, define them before running the app.
+
+```
+export API_KEY=<YOUR_API_KEY>
+export API_URL=https://api.twelvelabs.io/v1.1
+```
+
+Alternatively, if you are running the operator as a delegated operator, you can pass them in through the app. 
+
+Choose the index name and the embeddings you would like to include in your index. After execution, videos will be uploaded and index will be created. Additionally, a new field with Twelve Labs + Index Name will be added to your sample to correlate a sample with a Twelve Labs UID.
+
+After your index has been created, its time for search! To use semantic video search, input the index name you want to search on, the prompt you are searching through, and which embeddings to search through. Your index needs to have these embeddings in order to search. Afterwards, your dataset will be sorted based on the most similar samples!
+
+Here's a quick demo below!
+
+
+## Semantic Frames Search
 
 Any text similarity index should be computed before hand using [FiftyOne Brain's Compute Similarity](https://docs.voxel51.com/user_guide/brain.html#text-similarity). The similarity index should also be on the frames of the image, in order to properly generate image embeddings. 
 
@@ -39,3 +69,5 @@ results= fob.compute_similarity(
 
 session = fo.launch_app(frames) #run plugin
 ```
+
+Afterwards, choose to sort by frames or videos. This determines what type of view, video or frames, is returned. Pass a prompt in and sort your dataset!
--- __init__.py
+++ __init__.py
@@ -2,15 +2,21 @@ import fiftyone as fo
 import fiftyone.operators as foo
 from fiftyone.operators import types
 from fiftyone.brain import Similarity
+import time
+import requests
+import glob
+from pprint import pprint
+import os
 
 
-class SemanticVideoSearch(foo.Operator):
+
+class SemanticFrameSearch(foo.Operator):
     @property
     def config(self):
         return foo.OperatorConfig(
-            name="semantic_video_search",
-            label="semantic video search",
-            description="semantic video search",
+            name="semantic_frames_search",
+            label="semantic frames search",
+            description="semantic video search using only frames as context",
             dynamic=True,
             icon="/assets/search.svg",
         )
@@ -53,6 +59,328 @@ class SemanticVideoSearch(foo.Operator):
                     sorted_list.append(sample.sample_id)
             ctx.trigger("set_view", {"view": ctx.dataset.select(sorted_list,ordered=True)._serialize()})
         return {}
+
+class SemanticVideoBackend(foo.Operator):
+    @property
+    def config(self):
+        return foo.OperatorConfig(
+            name="create_semantic_video_index",
+            label="create semantic video index",
+            description="create a semantic video index backend with Twelve Labs",
+            dynamic=True,
+            icon="/assets/search.svg",
+        )
+    
+    def resolve_input(self, ctx):
+        inputs = types.Object()
+        target_view = get_target_view(ctx, inputs)
+        inputs.message(
+            "Notice", 
+            label="Create Semantic Video Index", 
+            description="Choose your index name and the modalities of embeddings you will use!"
+        )
+        inputs.str("index_name", label="Index_Name", required=True)
+        inputs.view(
+            "header", 
+            types.Header(label="Select modalities for index", description="Select one or more from the below to extract embeddings from your videos", divider=True)
+        )
+        inputs.bool(
+            "visual",
+            label="visual",
+            description="",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "conversation",
+            label="conversation",
+            description="Video must have audio to work!",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "text_in_video",
+            label="text_in_video",
+            description="",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "logo",
+            label="logo",
+            description="",
+            view=types.CheckboxView(),
+        )
+
+        inputs.view(
+            "header2", 
+            types.Header(label="Advise delegating to avoid timeout", description="", divider=True)
+        )
+        _execution_mode(ctx, inputs)
+        return types.Property(inputs)
+    
+    def resolve_delegation(self, ctx):
+        return ctx.params.get("delegate", False)
+    
+    def execute(self, ctx):
+        ctx.dataset.compute_metadata()
+        target = ctx.params.get("target", None)
+        target_view = _get_target_view(ctx, target)
+        API_URL = os.getenv("TWELVE_API_URL")
+
+        API_KEY = os.getenv("TWELVE_API_KEY")
+        if ctx.params.get("delegate", None):
+            API_URL = ctx.params.get("TWELVE_API_URL", None)
+
+            API_KEY = ctx.params.get("TWELVE_API_KEY", None)
+
+        INDEX_NAME = ctx.params.get("index_name")
+
+        INDEXES_URL = f"{API_URL}/indexes"
+
+        headers = {
+            "x-api-key": API_KEY
+        }
+        
+        so = []
+
+        if ctx.params.get("visual"):
+            so.append("visual")
+        if ctx.params.get("logo"):
+            so.append("logo")
+        if ctx.params.get("text_in_video"):
+            so.append("text_in_video")
+        if ctx.params.get("conversation"):
+            so.append("conversation")
+
+        data = {
+        "engine_id": "marengo2.5",
+        "index_options": so,
+        "index_name": INDEX_NAME,
+        }
+
+        response = requests.post(INDEXES_URL, headers=headers, json=data)
+
+        headers = {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json"
+        } 
+
+        INDEX_ID = get_twelve_id_from_name(INDEXES_URL, headers, INDEX_NAME)
+
+        TASKS_URL = f"{API_URL}/tasks"
+
+        videos = target_view
+        for sample in videos:
+            if sample.metadata.duration < 4:
+                continue
+            else:
+                file_name = sample.filepath.split("/")[-1] 
+                file_path = sample.filepath 
+                file_stream = open(file_path,"rb")
+            
+                headers = {
+                    "x-api-key": API_KEY
+                }
+            
+                data = {
+                    "index_id": INDEX_ID, 
+                    "language": "en"
+                }
+            
+                file_param=[
+                    ("video_file", (file_name, file_stream, "application/octet-stream")),]
+            
+                response = requests.post(TASKS_URL, headers=headers, data=data, files=file_param)
+                TASK_ID = response.json().get("_id")
+                print (f"Status code: {response.status_code}")
+                pprint (response.json())
+            
+                TASK_STATUS_URL = f"{API_URL}/tasks/{TASK_ID}"
+                while True:
+                    response = requests.get(TASK_STATUS_URL, headers=headers)
+                    STATUS = response.json().get("status")
+                    if STATUS == "ready":
+                        break
+                    time.sleep(10)
+                
+                VIDEO_ID = response.json().get('video_id')
+                sample["Twelve Labs " + INDEX_NAME] = VIDEO_ID
+                sample.save()
+        return {}
+
+
+class SemanticVideoSearch(foo.Operator):
+    @property
+    def config(self):
+        return foo.OperatorConfig(
+            name="semantic_video_search",
+            label="semantic video search",
+            description="semantic video search using videos as context",
+            dynamic=True,
+            icon="/assets/search.svg",
+        )
+    
+    def resolve_input(self, ctx):
+        inputs = types.Object()
+        inputs.message(
+            "Notice", 
+            label="Semantic Video Search", 
+            description="Search through your video dataset with a prompt. If you haven't yet \
+                 generated a similarity index with Twelve Labs, run the creaet semantic video index \
+                      operator first! Note, you can only search on modalities within your chosen index!"
+        )
+        inputs.str("index_name", label="Index_Name", required=True)
+        inputs.str("prompt", label="Prompt", required=True)
+
+        inputs.view(
+            "header", 
+            types.Header(label="Select modalities for search", description="Select one or more from the below to search through your videos. Note: your index must have this modality!", divider=True)
+        )
+        inputs.bool(
+            "visual",
+            label="visual",
+            description="",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "conversation",
+            label="conversation",
+            description="Video must have audio to work!",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "text_in_video",
+            label="text_in_video",
+            description="",
+            view=types.CheckboxView(),
+        )
+        inputs.bool(
+            "logo",
+            label="logo",
+            description="",
+            view=types.CheckboxView(),
+        )
+
+        inputs.view(
+            "header2", 
+            types.Header(label="Advise delegating to avoid timeout", description="", divider=True)
+        )
+        _execution_mode(ctx, inputs)
+        
+        return types.Property(inputs)
+    
+    def resolve_delegation(self, ctx):
+        return ctx.params.get("delegate", False)
+    
+    def execute(self, ctx):
+
+        API_URL = os.getenv("TWELVE_API_URL")
+        print(API_URL)
+
+        API_KEY = os.getenv("TWELVE_API_KEY")
+        print(API_KEY)
+
+        if ctx.params.get("delegate", None):
+            API_URL = ctx.params.get("TWELVE_API_URL", None)
+
+            API_KEY = ctx.params.get("TWELVE_API_KEY", None)
+
+        INDEX_NAME = ctx.params.get("index_name")
+
+        INDEXES_URL = f"{API_URL}/indexes"
+
+        headers = {
+            "x-api-key": API_KEY,
+            "Content-Type": "application/json"
+        } 
+
+        INDEX_ID = get_twelve_id_from_name(INDEXES_URL, headers, INDEX_NAME)
+        prompt = ctx.params.get("prompt")
+        SEARCH_URL = f"{API_URL}/search"
+
+        headers = {
+        "x-api-key": API_KEY
+        }
+
+        so = []
+
+        if ctx.params.get("visual"):
+            so.append("visual")
+        if ctx.params.get("logo"):
+            so.append("logo")
+        if ctx.params.get("text_in_video"):
+            so.append("text_in_video")
+        if ctx.params.get("conversation"):
+            so.append("conversation")
+
+        data = {
+        "query": prompt,
+        "index_id": INDEX_ID,
+        "search_options": so,
+        }
+
+        response = requests.post(SEARCH_URL, headers=headers, json=data)
+        video_ids = [entry['video_id'] for entry in response.json()['data']]
+        print(response.json())
+        ctx.trigger("set_view", {"view": ctx.dataset.select_by("Twelve Labs " + INDEX_NAME, video_ids,ordered=True)._serialize()})
+        return {}
+    
+def get_target_view(ctx, inputs):
+    has_view = ctx.view != ctx.dataset.view()
+    has_selected = bool(ctx.selected)
+    default_target = None
+
+    if has_view or has_selected:
+        target_choices = types.RadioGroup(orientation="horizontal")
+        target_choices.add_choice(
+            "DATASET",
+            label="Entire dataset",
+            description="Process the entire dataset",
+        )
+
+        if has_view:
+            target_choices.add_choice(
+                "CURRENT_VIEW",
+                label="Current view",
+                description="Process the current view",
+            )
+            default_target = "CURRENT_VIEW"
+
+        if has_selected:
+            target_choices.add_choice(
+                "SELECTED_SAMPLES",
+                label="Selected samples",
+                description="Process only the selected samples",
+            )
+            default_target = "SELECTED_SAMPLES"
+
+        inputs.enum(
+            "target",
+            target_choices.values(),
+            default=default_target,
+            required=True,
+            label="Target view",
+            view=target_choices,
+        )
+
+    target = ctx.params.get("target", default_target)
+
+    return _get_target_view(ctx, target)
+
+def _get_target_view(ctx, target):
+    if target == "SELECTED_SAMPLES":
+        return ctx.view.select(ctx.selected)
+
+    if target == "DATASET":
+        return ctx.dataset
+
+    return ctx.view
+
+def get_twelve_id_from_name(INDEXES_URL, headers, INDEX_NAME):
+    response = requests.get(INDEXES_URL, headers=headers)
+    INDEX_ID = None
+    for index in response.json()["data"]:
+        if index["index_name"] == INDEX_NAME:
+            INDEX_ID = index["_id"]
+    return INDEX_ID
 
 _BRAIN_RUN_TYPES = {
     "similarity": Similarity,
@@ -115,5 +443,42 @@ def get_brain_key(
 
     return ctx.params.get("brain_key", None)
 
+def _execution_mode(ctx, inputs):
+    delegate = ctx.params.get("delegate", False)
+
+    if delegate:
+        description = "Uncheck this box to execute the operation immediately"
+    else:
+        description = "Check this box to delegate execution of this task"
+
+    inputs.bool(
+        "delegate",
+        default=False,
+        required=True,
+        label="Delegate execution?",
+        description=description,
+        view=types.CheckboxView(),
+    )
+
+    if delegate:
+        inputs.view(
+            "notice",
+            types.Notice(
+                label=(
+                    "You've chosen delegated execution. Note that you must "
+                    "have a delegated operation service running in order for "
+                    "this task to be processed. See "
+                    "https://docs.voxel51.com/plugins/index.html#operators "
+                    "for more information"
+                )
+            ),
+        )
+        inputs.str("TWELVE_API_KEY", label="TWELVE_API_KEY", required=True)
+        inputs.str("TWELVE_API_URL", label="TWELVE_API_URL", required=True)
+
+    
+
 def register(plugin):
+    plugin.register(SemanticFrameSearch)
     plugin.register(SemanticVideoSearch)
+    plugin.register(SemanticVideoBackend)
